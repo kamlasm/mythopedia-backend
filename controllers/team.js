26fdb7d70e3fc2e00fcd2d2c203ddc350a066048
Melaniemsc <melsan1706@gmail.com>
2024-06-17 17:29:06 +0100

    created team component to update the database

--- controllers/team.js
+++ controllers/team.js
@@ -7,10 +7,32 @@ const router = express.Router()
 router.put('/your-team', secureRoute, async function editTeam(req, res) {
     try {
         const user = res.locals.currentUser
-        console.log(req.body)
+        const gameplay= user.gameplay
+
+
+        // await User.updateOne({id: user.gameplay._id},
+        //     {gameplay:{
+        //         money: req.body.money,
+        //         totalStrength: req.body.strength,
+        //         totalIntelligence: req.body.intelligence, 
+        //         team: req.body.team
+        //     }}
+        // )
+        console.log(user);
+        gameplay.money= req.body.newMoney,
+        gameplay.totalStrength= req.body.newStrength,
+        gameplay.totalIntelligence= req.body.newIntelligence,
+        gameplay.team= req.body.newTeam,
+
+        console.log("this is the gameplay info", gameplay)
+
+        await user.save()
+        console.log(user);
+        
+
 
     } catch (err) {
-
+        console.log(err)
     }
 })
 
